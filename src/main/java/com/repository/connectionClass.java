6c5fddd1568e4cafd7377bed0c7db7f5fb64bbef
AyeChay <93075349+AyeChay@users.noreply.github.com>
2024-06-25 22:47:32 +0630

    add project

--- src/main/java/com/repository/connectionClass.java
+++ src/main/java/com/repository/connectionClass.java
@@ -1,22 +1,23 @@
-package com.repository;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class connectionClass {
+
 	public static Connection getConnection()
 	{
 		Connection con = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/inventory_management_system","root", "Thandar@2018#!");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/inventory_management_system", "root", "admin");
 		} catch (ClassNotFoundException e) {
-			System.out.println(e.getMessage());
+			System.out.println("Loading class: "+ e.getMessage());
 		} catch (SQLException e) {
-			System.out.println(e.getMessage());
+			System.out.println("Database Connection: "+ e.getMessage());
 		}
-	
+		
 		return con;
 	}
+	
 }
